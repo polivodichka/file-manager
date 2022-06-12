4a908088658448a6bf5c246e3742ad0be4e65d83
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-06-12 13:06:44 +0300

    fix: use streams in hash

--- src/hash.js
+++ src/hash.js
@@ -4,12 +4,9 @@ import crypto from 'crypto';
 import { throwOperationFailed } from './errors.js';
 
 export const getHash = async (currentDirectory, fileName, readline) => {
-    await fs.readFile(path.resolve(currentDirectory, fileName), (error, fileBuffer) => {
-        if (error) {
-            throwOperationFailed('No such file!');
-        } else {
-            console.log(crypto.createHash('sha256').update(fileBuffer).digest('hex'));
-        }
-        readline.prompt();
-    });
+    const stream = fs.ReadStream(path.resolve(currentDirectory, fileName));
+
+    stream.on('error', () => throwOperationFailed('No such file in this directory!'));
+    stream.on('data', data => console.log(crypto.createHash('sha256').update(data).digest('hex')));
+    stream.on('close', () => readline.prompt());
 }
