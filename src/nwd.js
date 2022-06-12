6367b27c06920e1dbed0e15c8d0b140ed73d69c5
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-06-12 12:46:37 +0300

    fix: list command

--- src/nwd.js
+++ src/nwd.js
@@ -1,18 +1,19 @@
 
 import path from 'path';
 import fs from 'fs';
+import { throwOperationFailed } from './errors.js';
 
 export const up = (currentPath) => {
     return path.resolve(currentPath, '..')
 }
+
 export const cd = (currentPath, additional) => {
     const newPath = path.resolve(currentPath, additional);
-    return fs.existsSync(newPath) && list(newPath) ? newPath : currentPath;
+    if(!fs.existsSync(newPath)) throwOperationFailed('No such file or directory!');
+    else if(!fs.lstatSync(newPath).isDirectory()) throwOperationFailed('No such directory!');
+    return fs.existsSync(newPath) && fs.lstatSync(newPath).isDirectory() ? newPath : currentPath;
 }
-export const list = async (folder, readline) => {
-    return fs.readdir(folder, (err) => {
-        if (err) {
-            return false;
-        }
-    });
+
+export const list = async (folder) => {
+    return fs.promises.readdir(folder);
 }
