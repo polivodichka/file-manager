25618395fcadacddb9fec6cfe951b3a282e64796
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-06-12 13:22:40 +0300

    refactor: compress and decompress

--- src/zip.js
+++ src/zip.js
@@ -1,37 +1,34 @@
 import fs from 'fs';
 import path from 'path';
 import zlib from 'zlib';
+import { throwOperationFailed } from './errors.js';
 
 export const compress = ([toRead, toWrite]) => {
-    if (!(toRead && fs.existsSync(toWrite))){
-        try {
-            throw new Error('Error!')
-        } catch (error) {
-            console.log(error.message)
-            return;
-        }
+    console.log(toRead, fs.existsSync(toWrite))
+    if (toRead && !fs.existsSync(toWrite)) {
+        fs.mkdirSync(toWrite, () => { })
+    }
+    if (!(toRead && fs.existsSync(toWrite))) {
+        throwOperationFailed('No such file in this derictory!');
+        return;
     }
     const input = fs.createReadStream(toRead);
     const output = fs.createWriteStream(path.resolve(toWrite, `${path.basename(toRead)}.br`));
     const brotli = zlib.createBrotliCompress();
 
     input.pipe(brotli).pipe(output);
-    console.log(`Successfully compressed into ${path.basename(toRead)}.br`);
+    console.log(`Successfully compressed into ${path.resolve(toWrite, path.basename(toRead))}.br`);
 };
 
 export const decompress = ([toRead, toWrite]) => {
-    if (!(toRead && fs.existsSync(toWrite))){
-        try {
-            throw new Error('Error!')
-        } catch (error) {
-            console.log(error.message)
-            return;
-        }
+    if (!(toRead && fs.existsSync(toWrite))) {
+        throwOperationFailed('No such file in this derictory!');
+        return;
     }
     const input = fs.createReadStream(toRead);
     const output = fs.createWriteStream(path.resolve(toWrite, `${path.basename(toRead, path.extname(toRead))}`));
-    const brotli = zlib.createBrotliDecompress(); 
+    const brotli = zlib.createBrotliDecompress();
 
     input.pipe(brotli).pipe(output);
-    console.log(`Successfully decompressed into ${path.basename(toRead, path.extname(toRead))}`)
+    console.log(`Successfully decompressed into ${path.resolve(toWrite, path.basename(toRead, path.extname(toRead)))}`);
 };
