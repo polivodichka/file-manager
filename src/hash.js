714235db2c47953206aaed464cb6842becb93116
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-06-12 12:58:12 +0300

    refactor: hash

--- src/hash.js
+++ src/hash.js
@@ -1,11 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 import crypto from 'crypto';
+import { throwOperationFailed } from './errors.js';
 
 export const getHash = async (currentDirectory, fileName, readline) => {
     await fs.readFile(path.resolve(currentDirectory, fileName), (error, fileBuffer) => {
         if (error) {
-            console.log('Invalid input');
+            throwOperationFailed('No such file!');
         } else {
             console.log(crypto.createHash('sha256').update(fileBuffer).digest('hex'));
         }
